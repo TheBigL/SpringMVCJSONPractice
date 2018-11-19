66eadd34e73e76bda733c9fb9d2e1a379a24be3f
TheBigL <lmohamed1@studentmail.nait.ca>
2018-11-19 15:11:50 -0700

    Adding JAR files into Libraries and building Test Classes

--- src/com.lebanmohamed/controller/MainController.java
+++ src/com.lebanmohamed/controller/MainController.java
@@ -1,18 +1,17 @@
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 
 public class MainController
 {
-    @GetMapping("/")
-    public String IndexController(Model m)
+    public static void main(String[] args)
     {
-        m.addAttribute("someAttribute", "someValue");
-        return "index";
-
+        JSONController jsonController = new JSONController();
     }
+
 
 }
---
+++ src/com.lebanmohamed/test/JSONTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JSONTest
+{
+
+
+    private ObjectMapper objectMapper ;
+    @BeforeEach
+    public void setUp()
+    {
+        objectMapper = new ObjectMapper();
+
+
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        objectMapper = null;
+
+    }
+
+}
