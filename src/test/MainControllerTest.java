6626c57d0f222a877ba0ba5fe035958a2c7773d9
TheBigL <lmohamed1@studentmail.nait.ca>
2018-11-18 21:55:46 -0700

    Start of a sample Project

---
+++ src/test/MainControllerTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.annotation.processing.Generated;
+
+class MainControllerTest
+{
+    @BeforeClass
+    public void setUp()
+    {
+
+    }
+
+    @Test
+    @Generated("Return test")
+    public void returnTest() throws Exception
+    {
+
+    }
+
+    @AfterClass
+    public void tearDown()
+    {
+
+    }
+
+}
